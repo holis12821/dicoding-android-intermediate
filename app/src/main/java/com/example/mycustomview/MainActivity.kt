82e073fd3b39afa77cfffc3d2fe55c1ddd6e41fe
Nurholis <nur.kholis@ottencoffee.co.id>
2022-09-09 21:56:20 +0700

    Add custom view on exercise module 1

--- app/src/main/java/com/example/mycustomview/MainActivity.kt
+++ app/src/main/java/com/example/mycustomview/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.mycustomview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import androidx.core.content.ContextCompat
+import com.example.mycustomview.custom.MyButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: MyButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        button = findViewById(R.id.my_button)
+        setComponent()
+    }
+
+    private fun setComponent() {
+        button.setAttribute(
+            textColor = ContextCompat.getColor(this, R.color.white),
+            textSize = resources.getDimension(R.dimen.text_large),
+            gravity = Gravity.CENTER
+        )
+
     }
 }
