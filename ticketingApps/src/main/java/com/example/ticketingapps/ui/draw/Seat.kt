1615d51dd2cfc1acc987557c6b085196d24b33a0
Nurholis <nur.kholis@ottencoffee.co.id>
2022-09-11 20:23:24 +0700

    Add dynamicFeatures module and add customView SeatsView

---
+++ ticketingApps/src/main/java/com/example/ticketingapps/ui/draw/Seat.kt
@@ -0,0 +1,11 @@
+package com.example.ticketingapps.ui.draw
+
+data class Seat(
+    val id: Int,
+    var x: Float? = 0F,
+    var y: Float? = 0F,
+    var name: String,
+    var isBooked: Boolean
+)
+
+
